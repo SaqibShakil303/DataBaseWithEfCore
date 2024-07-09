ac8bf7a9445c35e30a4131a8118954ccd3fb0995
freak303 <mds168723@gmail.com>
2024-07-09 18:14:01 +0530

    installed EF Core and setup DbContext class

---
+++ DbOperationWithEfCore/DbOperationWithEfCore/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DbOperationWithEfCore.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        //to not use hard code and configure all setting to progrm.cs 
+        //to push all the setting of AppDbContext to DbContext we use *base* keyword to send value from child constructor to parent const
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
